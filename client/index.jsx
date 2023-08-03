9625c2170b9c92248bb4a3422a62f262d6e748be
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-08-03 18:58:27 +0100

    `hydrateRoot`

--- client/index.jsx
+++ client/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {
-  createRoot
+  hydrateRoot
 } from 'react-dom/client'
 
 import IndexPage from '#client/components/index-page'
 
 const app = document.getElementById('app')
 
-createRoot(app)
-  .render(
-    <IndexPage />
-  )
+hydrateRoot(
+  app,
+  <IndexPage />
+)
